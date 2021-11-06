81888757b9fcd839a19c2ee634cde3bc8d718f29
AsharaniH <asharani120697@gmail.com>
2021-11-06 18:33:30 +0530

    Even Numbers

---
+++ web-Automation/src/com/sgtesting/pageobjectmodel/EvenNumbers.java
@@ -0,0 +1,16 @@
+package com.sgtesting.pageobjectmodel;
+
+public class EvenNumbers {
+
+	public static void main(String[] args) {
+		for(int i=20;i<=40;i++)
+		{
+			if(i%2==0)
+			{
+				System.out.println(i);
+			}
+		}
+
+	}
+
+}
